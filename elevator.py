3c0ef4c3a211107a508f847a3b01821266bf3b64
Yutaroh Tanaka <mmmm7217@gmail.com>
2023-04-03 17:00:45 -0400

    GUI almost done

--- elevator.py
+++ elevator.py
@@ -3,10 +3,10 @@ import tkinter as tk
 from threading import *
 
 
-button_pushed = [0,0,0,0,0,0,0,0] # button for 1 ~ 6 floors
+button_pushed = [0,0,0,0,0,0,0,0] # button for 1 ~ 8 floors
 power_on_off = False
 current_state = 0
-state_list = ["idle","move up", "move down"]
+state_list = ["Idle","Move Up", "Move Down","Stop","Stop","Open Door","Open Door","Close Door","Close Door"]
 current_floor = 1
 movement = "idle"
 pow = False
@@ -19,85 +19,129 @@ def elevator():
 
 
 def fsm():
-    global current_state, state_list, button_pushed, movement, current_floor,label_1
-    global label_floor, canvas3, state_idel_c,state_md_c,state_mu_c
+    global current_state, state_list, button_pushed, movement, current_floor,label_1,label_floor
+    global canvas3, state_idel_c,state_md_c,state_mu_c
+    global state_u_stop_c, state_u_open_c, state_u_close_c
+    global state_d_stop_c, state_d_open_c, state_d_close_c
 
     while(True):
         if(power_on_off):
             if current_state == 0:
-                if cs() == "up":
+                if 1 in [x for x in button_pushed[current_floor:]]:
                     current_state = 1
-                    current_floor+=1
                     canvas3.itemconfig(state_idel_c, fill="WHITE")
                     canvas3.itemconfig(state_mu_c, fill= "ORANGE")
-                elif cs() == "down":
+                elif 1 in [x for x in button_pushed[:current_floor]]:
                     current_state = 2
-                    current_floor-=1
                     canvas3.itemconfig(state_idel_c, fill="WHITE")
                     canvas3.itemconfig(state_md_c, fill= "ORANGE")
-
             elif current_state == 1:
-                if cs() == "down":
+                if button_pushed[current_floor-1] == 1:
+                    b_l[current_floor-1].configure(highlightbackground="WHITE")
+                    b_u[current_floor-1].configure(highlightbackground="WHITE")
+                    b_d[current_floor-1].configure(highlightbackground="WHITE")
+                    current_state = 3
+                    canvas3.itemconfig(state_mu_c, fill="WHITE")
+                    canvas3.itemconfig(state_u_stop_c, fill= "ORANGE")
+                else:
+                    button_pushed[current_floor-1] = 0
+                    current_floor+=1
+            elif current_state == 2:
+                if button_pushed[current_floor-1] == 1:
+                    b_l[current_floor-1].configure(highlightbackground="WHITE")
+                    b_u[current_floor-1].configure(highlightbackground="WHITE")
+                    b_d[current_floor-1].configure(highlightbackground="WHITE") 
+                    current_state = 4
+                    canvas3.itemconfig(state_md_c, fill="WHITE")
+                    canvas3.itemconfig(state_d_stop_c, fill= "ORANGE")
+                else:
+                    button_pushed[current_floor-1] = 0
+                    current_floor-=1
+            elif current_state == 3:
+                time.sleep(1)
+                current_state = 5
+                canvas3.itemconfig(state_u_stop_c, fill="WHITE")
+                canvas3.itemconfig(state_u_open_c, fill= "ORANGE")
+            elif current_state == 4:
+                time.sleep(1)
+                current_state = 6
+                canvas3.itemconfig(state_d_stop_c, fill="WHITE")
+                canvas3.itemconfig(state_d_open_c, fill= "ORANGE")
+
+            elif current_state == 5:
+                time.sleep(2)
+                current_state = 7
+                canvas3.itemconfig(state_u_open_c, fill="WHITE")
+                canvas3.itemconfig(state_u_close_c, fill= "ORANGE")
+
+            elif current_state == 6:
+                time.sleep(2)
+                current_state = 8
+                canvas3.itemconfig(state_d_open_c, fill="WHITE")
+                canvas3.itemconfig(state_d_close_c, fill= "ORANGE")
+
+            elif current_state == 7:
+                if 1 in [x for x in button_pushed[current_floor:]]:
+                    current_state = 1
+                    current_floor+=1
+                    canvas3.itemconfig(state_u_close_c, fill="WHITE")
+                    canvas3.itemconfig(state_mu_c, fill= "ORANGE")
+                elif 1 in [x for x in button_pushed[:current_floor+1]]:
                     current_state = 2
                     current_floor-=1                    
-                    canvas3.itemconfig(state_mu_c, fill="WHITE")
+                    canvas3.itemconfig(state_u_close_c, fill="WHITE")
                     canvas3.itemconfig(state_md_c, fill= "ORANGE")
-                elif cs() == "idle":
-                    current_state = 0
-                    canvas3.itemconfig(state_mu_c, fill="WHITE")
-                    canvas3.itemconfig(state_idel_c, fill= "ORANGE")
                 else:
-                    current_floor+=1
+                    current_state = 0
+                    canvas3.itemconfig(state_u_close_c, fill="WHITE")
+                    canvas3.itemconfig(state_idel_c, fill= "ORANGE")
 
-            elif current_state == 2:
-                if cs() == "up":
+            elif current_state == 8:
+                if 1 in [x for x in button_pushed[:current_floor+1]]:
+                    current_state = 2
+                    current_floor-=1
+                    canvas3.itemconfig(state_d_close_c, fill="WHITE")
+                    canvas3.itemconfig(state_md_c, fill= "ORANGE")
+                elif 1 in [x for x in button_pushed[current_floor:]]:
                     current_state = 1
                     current_floor+=1                    
-                    canvas3.itemconfig(state_md_c, fill="WHITE")
+                    canvas3.itemconfig(state_d_close_c, fill="WHITE")
                     canvas3.itemconfig(state_mu_c, fill= "ORANGE")
-                elif cs() == "idle":
-                    current_state = 0
-                    canvas3.itemconfig(state_md_c, fill="WHITE")
-                    canvas3.itemconfig(state_idel_c, fill= "ORANGE")
                 else:
-                    current_floor-=1
-            print(state_list[current_state])
-            button_pushed[current_floor-1] = 0
-            print(current_floor)
+                    current_state = 0
+                    canvas3.itemconfig(state_d_close_c, fill="WHITE")
+                    canvas3.itemconfig(state_idel_c, fill= "ORANGE")
+
+
             print(button_pushed)
+            print(current_floor)
             label_1.configure(text=state_list[current_state])
             label_floor.configure(text = current_floor)
             time.sleep(1)
         
-        
-def cs():
-    global button_pushed, current_floor, movement 
-    if  current_state == 0 or current_state == 1:
-        if 1 in [x for x in button_pushed[current_floor:]]:
-            return "up"
-        elif 1 in [x for x in button_pushed[:current_floor]]:
-            return "down"
-        else:
-            return "idle" 
-    elif  current_state == 2:
-        if 1 in [x for x in button_pushed[:current_floor]]:
-            return "dwon"
-        elif 1 in [x for x in button_pushed[current_floor:]]:
-            return "up"
-        else:
-            return "idle"
+
     
 
-def press_button(arg):
+def press_button(arg,bk):
     global button_pushed
+    
     button_pushed[arg-1] = 1
+    match bk:
+        case 0:
+            b_l[arg-1].configure(highlightbackground="PINK")
+        case 1:
+            b_u[arg-1].configure(highlightbackground="PINK")
+        case 2:
+            b_d[arg-1].configure(highlightbackground="PINK")
 
 def onof():
     global power_on_off
     if(not power_on_off):
         power_on_off = True
+        bof.configure(highlightbackground="GREEN")
     else:
         power_on_off = False
+        bof.configure(highlightbackground="RED")
 
 def create_gui():
     global current_state, state_list, button_pushed, moveu, label_1, label_floor 
@@ -117,22 +161,22 @@ def create_gui():
     #floor buttons
     canvas1 = tk.Canvas(window,bg = "WHITE", width= 150, height=600)
 
-    b1u = tk.Button(window, text = "\u25B2", command = lambda: press_button(1))
-    b1d = tk.Button(window, text = "\u25BC", command = lambda: press_button(1))
-    b2u = tk.Button(window, text = "\u25B2", command = lambda: press_button(2))
-    b2d = tk.Button(window, text = "\u25BC", command = lambda: press_button(2))
-    b3u = tk.Button(window, text = "\u25B2", command = lambda: press_button(3))
-    b3d = tk.Button(window, text = "\u25BC", command = lambda: press_button(3))
-    b4u = tk.Button(window, text = "\u25B2", command = lambda: press_button(4))
-    b4d = tk.Button(window, text = "\u25BC", command = lambda: press_button(4))
-    b5u = tk.Button(window, text = "\u25B2", command = lambda: press_button(5))
-    b5d = tk.Button(window, text = "\u25BC", command = lambda: press_button(5))
-    b6u = tk.Button(window, text = "\u25B2", command = lambda: press_button(6))
-    b6d = tk.Button(window, text = "\u25BC", command = lambda: press_button(6))
-    b7u = tk.Button(window, text = "\u25B2", command = lambda: press_button(7))
-    b7d = tk.Button(window, text = "\u25BC", command = lambda: press_button(7))
-    b8u = tk.Button(window, text = "\u25B2", command = lambda: press_button(8))
-    b8d = tk.Button(window, text = "\u25BC", command = lambda: press_button(8))
+    b1u = tk.Button(window, highlightbackground="WHITE",text = "\u25B2", command = lambda: press_button(1,1))
+    b1d = tk.Button(window, highlightbackground="WHITE",text = "\u25BC", command = lambda: press_button(1,2))
+    b2u = tk.Button(window, highlightbackground="WHITE",text = "\u25B2", command = lambda: press_button(2,1))
+    b2d = tk.Button(window, highlightbackground="WHITE",text = "\u25BC", command = lambda: press_button(2,2))
+    b3u = tk.Button(window, highlightbackground="WHITE",text = "\u25B2", command = lambda: press_button(3,1))
+    b3d = tk.Button(window, highlightbackground="WHITE",text = "\u25BC", command = lambda: press_button(3,2))
+    b4u = tk.Button(window, highlightbackground="WHITE",text = "\u25B2", command = lambda: press_button(4,1))
+    b4d = tk.Button(window, highlightbackground="WHITE",text = "\u25BC", command = lambda: press_button(4,2))
+    b5u = tk.Button(window, highlightbackground="WHITE",text = "\u25B2", command = lambda: press_button(5,1))
+    b5d = tk.Button(window, highlightbackground="WHITE",text = "\u25BC", command = lambda: press_button(5,2))
+    b6u = tk.Button(window, highlightbackground="WHITE",text = "\u25B2", command = lambda: press_button(6,1))
+    b6d = tk.Button(window, highlightbackground="WHITE",text = "\u25BC", command = lambda: press_button(6,2))
+    b7u = tk.Button(window, highlightbackground="WHITE",text = "\u25B2", command = lambda: press_button(7,1))
+    b7d = tk.Button(window, highlightbackground="WHITE",text = "\u25BC", command = lambda: press_button(7,2))
+    b8u = tk.Button(window, highlightbackground="WHITE",text = "\u25B2", command = lambda: press_button(8,1))
+    b8d = tk.Button(window,highlightbackground="WHITE", text = "\u25BC", command = lambda: press_button(8,2))
     label_1st = tk.Label(window, padx = 7, pady = 17, text = "1", anchor=tk.W)
     label_2st = tk.Label(window, padx = 6, pady = 17, text = "2", anchor=tk.W)
     label_3st = tk.Label(window, padx = 5, pady = 17, text = "3", anchor=tk.W)
@@ -141,6 +185,10 @@ def create_gui():
     label_6st = tk.Label(window, padx = 5, pady = 17, text = "6", anchor=tk.W)
     label_7st = tk.Label(window, padx = 6, pady = 17, text = "7", anchor=tk.W)
     label_8st = tk.Label(window, padx = 5, pady = 17, text = "8", anchor=tk.W)
+    
+    global b_u, b_d
+    b_u = [b1u,b2u,b3u,b4u,b5u,b6u,b7u,b8u]
+    b_d = [b1d,b2d,b3d,b4d,b5d,b6d,b7d,b8d]
 
 
     canvas1.create_window(73,518, anchor=tk.W, window=b1u)
@@ -169,19 +217,20 @@ def create_gui():
     canvas1.create_window(50,41, anchor=tk.W, window=label_8st)
 
     #elevator
+    global canvas2, b1, b2, b3, b4, b5, b6, b7, b8,bof
     canvas2 = tk.Canvas(window,bg = "WHITE", width= 500, height=600)
 
-    b1 = tk.Button(window, pady= 15, padx = 10, text = "1", command = lambda: press_button(1))
-    b2 = tk.Button(window, pady = 15, padx = 10,text = "2", command = lambda: press_button(2))
-    b3 = tk.Button(window, pady = 15, padx = 10, text = "3", command = lambda: press_button(3))
-    b4 = tk.Button(window, pady = 15, padx = 10, text = "4", command = lambda: press_button(4))
-    b5 = tk.Button(window, pady = 15, padx =10, text = "5", command = lambda: press_button(5))
-    b6 = tk.Button(window, pady = 15, padx =10, text = "6", command = lambda: press_button(6))
-    b7 = tk.Button(window, pady = 15, padx =10, text = "7", command = lambda: press_button(7))
-    b8 = tk.Button(window, pady = 15, padx =10, text = "8", command = lambda: press_button(8))
-    bo = tk.Button(window, pady = 15, padx =5, text = "<>")
-    bc = tk.Button(window, pady = 15, padx =5, text = "><")
-    bof = tk.Button(window, pady = 15, padx =5, text = "power", command = onof)
+    b1 = tk.Button(window, pady= 15, highlightbackground="WHITE",padx = 10, text = "1", command = lambda: press_button(1,0))
+    b2 = tk.Button(window, pady = 15, highlightbackground="WHITE",padx = 10,text = "2", command = lambda: press_button(2,0))
+    b3 = tk.Button(window, pady = 15, highlightbackground="WHITE",padx = 10, text = "3", command = lambda: press_button(3,0))
+    b4 = tk.Button(window, pady = 15, highlightbackground="WHITE",padx = 10, text = "4", command = lambda: press_button(4,0))
+    b5 = tk.Button(window, pady = 15, highlightbackground="WHITE",padx =10, text = "5", command = lambda: press_button(5,0))
+    b6 = tk.Button(window, pady = 15, highlightbackground="WHITE",padx =10, text = "6", command = lambda: press_button(6,0))
+    b7 = tk.Button(window, pady = 15, highlightbackground="WHITE",padx =10, text = "7", command = lambda: press_button(7,0))
+    b8 = tk.Button(window, pady = 15, highlightbackground="WHITE",padx =10, text = "8", command = lambda: press_button(8,0))
+    bo = tk.Button(window, pady = 15, highlightbackground="WHITE",padx =5, text = "<>")
+    bc = tk.Button(window, pady = 15, highlightbackground="WHITE",padx =5, text = "><")
+    bof = tk.Button(window, pady = 15, highlightbackground="RED",padx =5, text = "power", command = onof)
     label_1 = tk.Label(window, width= 30, padx = 65, pady = 5, text = "Power off ...", anchor=tk.W)
     label_floor = tk.Label(window, padx = 10, pady = 10,  font=("Arial", 32), text = "1", anchor=tk.W)
 
@@ -198,21 +247,21 @@ def create_gui():
     canvas2.create_window(20,560,anchor=tk.W, window=bof)
     canvas2.create_window(50,50, anchor=tk.NW, window=label_1)
     canvas2.create_window(50,120, anchor=tk.NW, window=label_floor)
-
-
+    global b_l
+    b_l = [b1,b2,b3,b4,b5,b6,b7,b8]
 
     #FSM
     global state_idel_c, state_md_c, state_mu_c,canvas3
+    global state_u_stop_c,state_u_open_c,state_u_close_c,state_d_stop_c,state_d_open_c,state_d_close_c
+
     canvas3 = tk.Canvas(window, bg ="WHITE", width=500, height=600)
    
     state_idel_c = canvas3.create_oval(200,20,300,80, outline = "black", fill = "Orange", width = 2)
     state_md_c = canvas3.create_oval(80,120,180,180, outline = "black", fill = "WHITE", width = 2)
-    state_d_arrive_c = canvas3.create_oval(80,220,180,280, outline = "black", fill = "WHITE", width = 2)
     state_d_stop_c = canvas3.create_oval(80,320,180,380, outline = "black", fill = "WHITE", width = 2)
     state_d_open_c = canvas3.create_oval(80,420,180,480, outline = "black", fill = "WHITE", width = 2)
     state_d_close_c = canvas3.create_oval(80,520,180,580, outline = "black", fill = "WHITE", width = 2)
     state_mu_c = canvas3.create_oval(320,120,420,180, outline = "black", fill = "WHITE", width = 2)
-    state_u_arrive_c = canvas3.create_oval(320,220,420,280, outline = "black", fill = "WHITE", width = 2)
     state_u_stop_c = canvas3.create_oval(320,320,420,380, outline = "black", fill = "WHITE", width = 2)
     state_u_open_c = canvas3.create_oval(320,420,420,480, outline = "black", fill = "WHITE", width = 2)
     state_u_close_c = canvas3.create_oval(320,520,420,580, outline = "black", fill = "WHITE", width = 2)
@@ -220,11 +269,9 @@ def create_gui():
     label_idle = tk.Label(window, text = "Idle", fg= "black", bg = "WHITE", anchor=tk.W)
     label_md = tk.Label(window, text = "Move Down", fg = "black",bg = "WHITE", anchor=tk.W)
     label_mu = tk.Label(window, text = "Move Up", fg="black", bg = "WHITE", anchor=tk.W)
-    label_d_arrive = tk.Label(window, text = "Arrive", fg= "black", bg = "WHITE", anchor=tk.W)
     label_d_stop = tk.Label(window, text = "Stop", fg = "black",bg = "WHITE", anchor=tk.W)
     label_d_open = tk.Label(window, text = "Open Door", fg="black", bg = "WHITE", anchor=tk.W)
     label_d_close = tk.Label(window, text = "Close Door", fg= "black", bg = "WHITE", anchor=tk.W)
-    label_u_arrive = tk.Label(window, text = "Arrive", fg = "black",bg = "WHITE", anchor=tk.W)
     label_u_stop = tk.Label(window, text = "Stop", fg="black", bg = "WHITE", anchor=tk.W)
     label_u_open = tk.Label(window, text = "Open Door", fg= "black", bg = "WHITE", anchor=tk.W)
     label_u_close = tk.Label(window, text = "Close Door", fg = "black",bg = "WHITE", anchor=tk.W)
@@ -232,11 +279,9 @@ def create_gui():
     canvas3.create_window(240,40, anchor=tk.NW, window=label_idle)
     canvas3.create_window(95,140, anchor=tk.NW, window=label_md)
     canvas3.create_window(340,140, anchor=tk.NW, window=label_mu)
-    canvas3.create_window(95,240, anchor=tk.NW, window=label_d_arrive)
     canvas3.create_window(95,340, anchor=tk.NW, window=label_d_stop)
     canvas3.create_window(95,440, anchor=tk.NW, window=label_d_open)
     canvas3.create_window(95,540, anchor=tk.NW, window=label_d_close)
-    canvas3.create_window(340,240, anchor=tk.NW, window=label_u_arrive)
     canvas3.create_window(340,340, anchor=tk.NW, window=label_u_stop)
     canvas3.create_window(340,440, anchor=tk.NW, window=label_u_open)
     canvas3.create_window(340,540, anchor=tk.NW, window=label_u_close)
